059b722505c74111e06751e48ef712e9b009a68b
姜皓育 <1332141191@qq.com>
2018-09-27 21:06:59 +0800

    初步完成

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //创建表格
-creatTable(8,8)
-function creatTable(X,Y) {
+createTable(8,8)
+function createTable(X,Y) {
     var table = document.getElementById("table")
     var tableNode = document.createElement("table")
     tableNode.style="background:#ccc;border-spacing:0px;display:inline-table;margin-top:20px"
@@ -9,17 +9,127 @@ function creatTable(X,Y) {
         var trNode = tableNode.insertRow()
         for(let y=0;y<Y;y++){
             var tdNode = trNode.insertCell()
+            tdNode.classList.add("hover")
             tdNode.style=tdNode_style
         }
     }
     table.appendChild(tableNode)
-
-    var a = Math.floor(Math.random()*8)
-    var b = Math.floor(Math.random()*8)
-    var tr = document.getElementsByTagName("tr")
-    console.log(tr[a])
-    tr[a][b].background="red"
 }
+createLei(8,8,10)
+function createLei(X,Y,N) {
+    var randomX
+    var randomY
+    var table
+    var tr
+    var td
+    for(let i=0;i<N;i++){
+        select_td()
+        function select_td(){
+            randomX = Math.floor(Math.random()*X)
+            randomY = Math.floor(Math.random()*Y)
+            table = document.getElementsByTagName("tr")
+            tr = table[randomY].childNodes
+            td = tr[randomX]
+        }
+        test_td()
+        function test_td() {
+            if(td.classList.contains("lei")){
+                select_td()
+                test_td()
+            }else{
+                td.classList.add("lei")
+            }
+        }
+    }
+    for(let i=0;i<Y;i++){
+        tr = table[i].childNodes
+        for (let j=0;j<X;j++){
+            var num = 0
+            td = tr[j]
+            if (td.classList.contains("lei")==false){
+                if(i-1>=0&&j-1>=0){
+                    let trFind = table[(i-1)].childNodes
+                    let tdFind = trFind[(j-1)]
+                    if(tdFind.classList.contains("lei")){
+                        num++
+                    }
+                }//判断左上
+                if(i-1>=0){
+                    let trFind = table[(i-1)].childNodes
+                    let tdFind = trFind[j]
+                    if(tdFind.classList.contains("lei")){
+                        num++
+                    }
+                }//判断上
+                if(i-1>=0&&j+1<X){
+                    let trFind = table[(i-1)].childNodes
+                    let tdFind = trFind[(j+1)]
+                    if(tdFind.classList.contains("lei")){
+                        num++
+                    }
+                }//判断右上
+                if(j-1>=0){
+                    let trFind = table[i].childNodes
+                    let tdFind = trFind[(j-1)]
+                    if(tdFind.classList.contains("lei")){
+                        num++
+                    }
+                }//判断左
+                if(j+1<X){
+                    let trFind = table[i].childNodes
+                    let tdFind = trFind[(j+1)]
+                    if(tdFind.classList.contains("lei")){
+                        num++
+                    }
+                }//判断右
+                if(i+1<Y&&j-1>=0){
+                    let trFind = table[(i+1)].childNodes
+                    let tdFind = trFind[(j-1)]
+                    if(tdFind.classList.contains("lei")){
+                        num++
+                    }
+                }//判断左下
+                if(i+1<Y){
+                    let trFind = table[(i+1)].childNodes
+                    let tdFind = trFind[j]
+                    if(tdFind.classList.contains("lei")){
+                        num++
+                    }
+                }//判断下
+                if(i+1<Y&&j+1<X){
+                    let trFind = table[(i+1)].childNodes
+                    let tdFind = trFind[(j+1)]
+                    if(tdFind.classList.contains("lei")){
+                        num++
+                    }
+                }
+                td.classList.add(num)
+            }
+        }
+    }
+}
+var td = document.getElementsByTagName("td")
+for(let i=0;i<td.length;i++){
+    td[i].addEventListener("click",function () {
+        if (this.classList.contains("lei")==false){
+            this.classList.remove("hover")
+            this.style.cssText="background: #bbb;border:1px solid #808080 !important;width:24px;height:24px"
+            if(this.classList.contains("1")){this.innerText="1"}
+            if(this.classList.contains("2")){this.innerText="2"}
+            if(this.classList.contains("3")){this.innerText="3"}
+            if(this.classList.contains("4")){this.innerText="4"}
+            if(this.classList.contains("5")){this.innerText="5"}
+            if(this.classList.contains("6")){this.innerText="6"}
+            if(this.classList.contains("7")){this.innerText="7"}
+            if(this.classList.contains("7")){this.innerText="8"}
+        } else {
+            var lei = document.getElementsByClassName("lei")
+            for (let i=0;i<lei.length;i++){
+                lei[i].style.cssText+="background:red"
+            }
 
+        }
+    })
+}
 
 
